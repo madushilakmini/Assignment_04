bc790deb141d7e92677c3f51ec49c41f526fe0ab
madushilakmini <139555956+madushilakmini@users.noreply.github.com>
2024-11-24 14:22:14 +0000

    add data file

---
+++ animal_matching_game/src/data/AnimalDb.js
@@ -0,0 +1,21 @@
+const animals = [
+    { id: 1, name: 'Dog', image: '1.jpg' },
+    { id: 2, name: 'Rabbit', image: '2.jpg' },
+    { id: 3, name: 'Tiger', image: '3.jpg' },
+    { id: 4, name: 'Giraffe', image: '4.jpg' },
+    { id: 5, name: 'Lion', image: '5.jpg' },
+    { id: 6, name: 'Panda', image: '6.jpg' },
+    { id: 7, name: 'Owl', image: '7.jpg' },
+    { id: 8, name: 'Elephant', image: '8.jpg' },
+    { id: 9, name: 'Cat', image: '9.jpg' },
+    { id: 10, name: 'Sheep', image: '10.jpg' },
+    { id: 11, name: 'Donkey', image: '11.jpg' },
+    { id: 12, name: 'Fox', image: '12.jpg' },
+    { id: 13, name: 'Wolf', image: '13.jpg' },
+    { id: 14, name: 'Bear', image: '14.jpg' },
+    { id: 15, name: 'Hen', image: '15.jpg' },
+    { id: 16, name: 'Goat', image: '16.jpg' },
+  ];
+  
+  export default animals;
+  
